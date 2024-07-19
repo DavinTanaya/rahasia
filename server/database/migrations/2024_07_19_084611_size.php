a0fff4ef28f2333e6daa07ceae1aca47e0b59cb9
DavinTanaya <davin.tanaya@gmail.com>
2024-07-19 20:43:57 +0700

    db

---
+++ server/database/migrations/2024_07_19_084611_size.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sizes', function (Blueprint $table) {
+            $table->id();
+            $table->enum('size', ['xs', 's', 'm', 'l', 'xl', '2xl', '3xl', 'free size']);
+            $table->foreignId('category_id')->references('id')->on('categories')->cascadeOnDelete()->cascadeOnUpdate();;
+            $table->foreignId('product_id')->references('id')->on('products')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->float('chest');
+            $table->float('waist');
+            $table->float('hip');
+            $table->float('length');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sizes');
+    }
+};
---
+++ server/database/migrations/2024_07_19_084648_address.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('address', function (Blueprint $table) {
+            $table->id();
+            $table->string('recipient');
+            $table->string('address');
+            $table->string('description');
+            $table->string('city');
+            $table->string('province');
+            $table->string('zip_code');
+            $table->string('phone_number');
+            $table->foreignId('user_id')->references('id')->on('users')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('address');
+    }
+};
